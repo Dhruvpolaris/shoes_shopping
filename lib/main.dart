69496231fad487c843041cb199248e899c05243a
dhruvpolaris <dhruv.phulsundar@gmail.com>
2020-06-19 01:10:51 +0530

    project setup

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: HomePage(),
+      ),
+    );
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
